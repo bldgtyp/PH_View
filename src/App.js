ce9308fe77d5ba669a16dcb5689615a7a1d6410f
PH Tools <phtools@bldgtyp.com>
2024-01-02 16:21:13 -0500

    main

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Use environment variable for API URL
+    const apiUrl =
+      process.env.REACT_APP_API_URL || "https://ph-view.onrender.com/";
+
+    console.log("API URL:", apiUrl); // Add this line
+
+    // Fetch data from the API
+    fetch(apiUrl)
+      .then((response) => {
+        // Check if the response is successful (status code 200)
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        // Parse the JSON response
+        return response.json();
+      })
+      .then((data) => {
+        // Set the message from the API response
+        setMessage(data.message);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []); // Empty dependency array ensures the effect runs once on mount
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{message}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
