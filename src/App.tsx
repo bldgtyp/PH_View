85c61fb5672c2fe46a361b55375ae697cd706a70
PH Tools <phtools@bldgtyp.com>
2024-01-03 12:14:07 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import { Tooltip } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import LinkIcon from "@mui/icons-material/Link";
 
 type ErvFields = {
   "AIRFLOW [CFM]": number;
@@ -22,6 +25,8 @@ type ErvFields = {
   "ROOMS SERVED": Array<string>;
   "WATTAGE [W]": number;
   "WINTER DEFROST PROTECTION?": string;
+  LINK: string;
+  "CUT SHEET": string;
 };
 
 type ErvRecord = {
@@ -61,16 +66,107 @@ function App() {
       });
   }, []); // Empty dependency array ensures the effect runs once on mount
 
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 150 },
+    { field: "MODEL", headerName: "Model", width: 130 },
+    { field: "MANUFACTURER", headerName: "Manufacturer", width: 130 },
+    {
+      field: "HEAT RECOVERY [%]",
+      headerName: "HR [%]",
+      width: 100,
+      valueFormatter: (params) => {
+        const value = params.value as number;
+        return `${Math.round(value * 100)}%`;
+      },
+      renderHeader: (params) => (
+        <Tooltip title="Heat Recovery Efficiency">
+          <span>{params.colDef.headerName}</span>
+        </Tooltip>
+      ),
+    },
+    {
+      field: "ENERGY RECOVERY [%]",
+      headerName: "ER [%]",
+      width: 100,
+      valueFormatter: (params) => {
+        const value = params.value as number;
+        return `${Math.round(value * 100)}%`;
+      },
+      renderHeader: (params) => (
+        <Tooltip title="Energy/Moisture Recovery Efficiency">
+          <span>{params.colDef.headerName}</span>
+        </Tooltip>
+      ),
+    },
+    {
+      field: "AIRFLOW [CFM]",
+      headerName: "Airflow [CFM]",
+      type: "number",
+      width: 100,
+    },
+    {
+      field: "LINK",
+      headerName: "Link",
+      width: 130,
+      renderCell: (params) => {
+        if (params.value) {
+          return (
+            <a
+              href={params.value as string}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LinkIcon />
+            </a>
+          );
+        } else {
+          return "-";
+        }
+      },
+    },
+    {
+      field: "CUT SHEET",
+      headerName: "Have Cut Sheet?",
+      width: 130,
+      renderCell: (params) => {
+        return params.value ? "Yes" : "No";
+      },
+      renderHeader: (params) => (
+        <Tooltip title="Does the Unit have a Design-Specification?">
+          <span>{params.colDef.headerName}</span>
+        </Tooltip>
+      ),
+    },
+  ];
+
+  const rows = ervData.map((item: ErvRecord) => {
+    return {
+      id: item.fields.DISPLAY_NAME,
+      MODEL: item.fields.MODEL,
+      MANUFACTURER: item.fields.MANUFACTURER,
+      "HEAT RECOVERY [%]": item.fields["HEAT RECOVERY [%]"],
+      "ENERGY RECOVERY [%]": item.fields["ENERGY RECOVERY [%]"],
+      "AIRFLOW [CFM]": item.fields["AIRFLOW [CFM]"],
+      LINK: item.fields["LINK"],
+      "CUT SHEET": item.fields["CUT SHEET"],
+    };
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        {ervData &&
-          Object.values(ervData).map((item: ErvRecord) => (
-            <div key={item.id}>
-              {item.createdTime}: {item.fields.DISPLAY_NAME}
-            </div>
-          ))}
-      </header>
+      <div style={{ height: "100%", width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 10 },
+            },
+          }}
+          pageSizeOptions={[10, 100]}
+          checkboxSelection
+        />
+      </div>
     </div>
   );
 }
