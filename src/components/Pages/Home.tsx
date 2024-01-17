789e84a1dc47c7c2a69d00dfc746c8d091f97844
PH Tools <phtools@bldgtyp.com>
2024-01-17 12:29:46 -0500

    Update Home.tsx

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -1,53 +1,55 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import fetchData from "../fetchAirTable";
+// import fetchData from "../fetchAirTable";
 import Page from "./Page";
 import ContentBlock from "../ContentBlock";
 import HomeCertificationStatus from "./HomeCertificationStatus";
 import HomeCertificationLinks from "./HomeCertificationLinks";
 import HomeCertificationNeeded from "./HomeCertificationNeeded";
 
-type AirTableRecord = { id: string; fields: { FIELD_NAME?: string; SECTION?: string; VALUE?: string } };
+// type AirTableRecord = { id: string; fields: { FIELD_NAME?: string; SECTION?: string; VALUE?: string } };
 
-function flattenData(d: AirTableRecord[]) {
-  let flatData: Record<string, string | undefined> = {};
-  d.forEach((item) => {
-    if (item.fields !== undefined && item.fields.FIELD_NAME !== undefined) {
-      flatData[item.fields.FIELD_NAME] = item.fields.VALUE;
-    }
-  });
-  return flatData;
-}
+// function flattenData(d: AirTableRecord[]) {
+//   let flatData: Record<string, string | undefined> = {};
+//   d.forEach((item) => {
+//     if (item.fields !== undefined && item.fields.FIELD_NAME !== undefined) {
+//       flatData[item.fields.FIELD_NAME] = item.fields.VALUE;
+//     }
+//   });
+//   return flatData;
+// }
 
 function Home() {
   let { projectId } = useParams();
-  const [certStatusData, setCertStatusData] = useState({});
-  const [certLinkData, setCertLinkData] = useState({});
-  const [certProjectData, setCertProjectData] = useState({});
+  console.log(">", projectId);
+  // const [certStatusData, setCertStatusData] = useState({});
+  // const [certLinkData, setCertLinkData] = useState({});
+  // const [certProjectData, setCertProjectData] = useState({});
 
-  useEffect(() => {
-    // const fetchProjectData = async () => {
-    //   const d = await fetchData(`${projectId}/config`);
-    //   // handle the fetched data
-    //   setCertStatusData(flattenData(d.filter((item: any) => item.fields.SECTION === "CERT_STATUS")));
-    //   setCertLinkData(flattenData(d.filter((item: any) => item.fields.SECTION === "LINKS")));
-    //   setCertProjectData(flattenData(d.filter((item: any) => item.fields.SECTION === "PROJ_DATA")));
-    // };
-    // fetchProjectData();
-  }, [projectId]);
+  // useEffect(() => {
+  //   const fetchProjectData = async () => {
+  //     const d = await fetchData(`${projectId}/config`);
+  //     // handle the fetched data
+  //     setCertStatusData(flattenData(d.filter((item: any) => item.fields.SECTION === "CERT_STATUS")));
+  //     setCertLinkData(flattenData(d.filter((item: any) => item.fields.SECTION === "LINKS")));
+  //     setCertProjectData(flattenData(d.filter((item: any) => item.fields.SECTION === "PROJ_DATA")));
+  //   };
+  //   // fetchProjectData();
+  // }, [projectId]);
 
   return (
-    <Page>
-      <ContentBlock>
-        <HomeCertificationStatus statusData={certStatusData} linkData={certLinkData} projData={certProjectData} />
-      </ContentBlock>
-      <ContentBlock>
-        <HomeCertificationLinks statusData={certStatusData} linkData={certLinkData} projData={certProjectData} />
-      </ContentBlock>
-      <ContentBlock>
-        <HomeCertificationNeeded statusData={certStatusData} linkData={certLinkData} projData={certProjectData} />
-      </ContentBlock>
-    </Page>
+    <>Test</>
+    // <Page>
+    //   <ContentBlock>
+    //     <HomeCertificationStatus statusData={certStatusData} linkData={certLinkData} projData={certProjectData} />
+    //   </ContentBlock>
+    //   <ContentBlock>
+    //     <HomeCertificationLinks statusData={certStatusData} linkData={certLinkData} projData={certProjectData} />
+    //   </ContentBlock>
+    //   <ContentBlock>
+    //     <HomeCertificationNeeded statusData={certStatusData} linkData={certLinkData} projData={certProjectData} />
+    //   </ContentBlock>
+    // </Page>
   );
 }
 
